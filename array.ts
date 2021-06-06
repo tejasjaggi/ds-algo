ebeb44d5eff4e20d4a141faa1b0c285662e4b3d5
Tejas Jaggi <[tejaggi@cisco.com]>
2021-06-06 14:54:34 +0530

    array practice

---
+++ array.ts
@@ -0,0 +1,17 @@
+// array.unshift() :-insert an element into the first position of array.
+//works by iterating over the array and moving every element to the next position
+//opposite of arr.push()
+
+let arr = [1,2,3,4]
+
+console.log("ARR1", arr)
+
+arr.unshift(0)
+
+console.log("ARR2", arr)
+
+
+//array.shift() :' remove the first element from the array
+//opposite of array.pop()\
+
+
--- practice.ts
+++
@@ -1,3 +0,0 @@
-
-
-console.log("inside the code")
